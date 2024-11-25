f0c8f3bbce72be7d878b56ded8e6625bcd8f4ff3
Arnold Andreasson <arnold@mellifica.se>
2024-11-25 12:06:05 +0100

    Cleanup and template code for Firebase deployment.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 import 'src/app/pages.dart' as app;
 
 void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   app.startApp();
 }
